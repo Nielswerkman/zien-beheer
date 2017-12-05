f0b7058eda594f8ad3ae715fca52ee135531e851
Niels Werkman <Nielswerkman93@gmail.com>
2017-12-05 10:34:34 +0100

    project setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { StageComponent } from './stage/stage.component';
+import { InternshipComponent } from './internship/internship.component';
+import { StudentComponent } from './student/student.component';
+import { BlogComponent } from './blog/blog.component';
+import { InstitutionComponent } from './institution/institution.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StageComponent,
+    InternshipComponent,
+    StudentComponent,
+    BlogComponent,
+    InstitutionComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule
