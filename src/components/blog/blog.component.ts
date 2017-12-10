5cd0f20d2940ef1900cbd507b58454530ae0ff08
IJpelaar,Bono B.G.M <ijpelaar_9@hotmail.com>
2017-12-10 13:28:25 +0100

    Removed unnecessary code for routing

--- src/components/blog/blog.component.ts
+++ src/components/blog/blog.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-//import {Router} from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 
 @Component({
@@ -9,7 +8,7 @@ import { Observable } from 'rxjs/Observable';
 })
 export class BlogComponent implements OnInit {
 
-  constructor(/*private router: Router*/) { }
+  constructor() { }
 
   ngOnInit() {
   }
