93b073c31312df444d97c61b2f02f474dfd2a324
IJpelaar,Bono B.G.M <ijpelaar_9@hotmail.com>
2017-12-12 14:55:18 +0100

    Added component add institution.

---
+++ src/components/institution/add-institution.component.ts
@@ -0,0 +1,12 @@
+import { OnInit, Component } from "@angular/core";
+
+Component({
+    selector: 'add-institution',
+    templateUrl: './add-institution.component.html',
+    styleUrls: ['./add-institution.component.css']
+})
+export class AddInstitutionComponent implements OnInit {
+    ngOnInit() {
+
+    }
+}
