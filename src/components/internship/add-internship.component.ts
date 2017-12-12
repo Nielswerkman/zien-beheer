4f0ce87a0eea9eb9883acc19c6b164bc527e70c5
IJpelaar,Bono B.G.M <ijpelaar_9@hotmail.com>
2017-12-12 14:29:05 +0100

    Added classes for add-internship.

---
+++ src/components/internship/add-internship.component.ts
@@ -0,0 +1,16 @@
+import { OnInit } from "@angular/core/src/metadata/lifecycle_hooks";
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'add-internship',
+    templateUrl: './add-internship.component.html',
+    styleUrls: ['./add-internship.component.css']
+})
+export class AddInternshipComponent implements OnInit {
+
+
+
+    ngOnInit() {
+
+    }
+}
