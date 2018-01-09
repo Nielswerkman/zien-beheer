b1c428a3573fa6f7a81ccbf7709699c256757d6b
Niels Werkman <Nielswerkman93@gmail.com>
2018-01-09 12:38:58 +0100

    update user component added

--- src/components/user/user.component.ts
+++ src/components/user/user.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {User} from '../../models/user';
 import {LiveUserService} from '../../services/UserService/LiveUserService';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -10,10 +11,14 @@ import {LiveUserService} from '../../services/UserService/LiveUserService';
 export class UserComponent implements OnInit {
   public users: User[] = [];
 
-  constructor(public UserService: LiveUserService) {
+  constructor(public UserService: LiveUserService, private route: Router) {
     UserService.getAll().subscribe(result => this.users = result);
   }
 
   ngOnInit() {
+  }
+
+  goToEdit(id: number){
+    this.route.navigate(['../user/update/', id]);
   }
 }
