3607c5a481e357818b7e56cf5946c385f99dcae1
IJpelaar,Bono B.G.M <ijpelaar_9@hotmail.com>
2018-01-09 15:24:08 +0100

    Changes to add internship page and typescript

--- src/components/internship/add-internship.component.ts
+++ src/components/internship/add-internship.component.ts
@@ -2,6 +2,7 @@ import { OnInit } from "@angular/core/src/metadata/lifecycle_hooks";
 import { Component } from "@angular/core";
 import { Institution } from "models/institution";
 import { LiveInstitutionService } from "services/InstitutionService/LiveInstitutionService";
+import { Internship } from "models/Internship";
 
 @Component({
     selector: 'add-internship',
@@ -12,11 +13,23 @@ export class AddInternshipComponent implements OnInit {
 
     private institutions: Institution[];
 
+    internship: Internship;
+
+    name = "";
+    type = "";
+    dateStart = null;
+    dateEnd = null;
+    institution = null;
+
     constructor(private institutionService: LiveInstitutionService) {}
 
     ngOnInit() {
         this.institutionService.getAll().subscribe(res => {
             this.institutions = res;
         })
+    }
+
+    postInternship() {
+        this.internship = new Internship();
     }
 }
